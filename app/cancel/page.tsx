eb6d7e3cdf3dcc43a0e801a01f1d40f18e7cdcf4
Abukstech <abdulmajeedbamidele4@gmail.com>
2024-07-17 22:33:58 +0100

    implemented stripe redirect url

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
-const page = () => {
-  return <div>Failed</div>;
+const TicketCancel = () => {
+  const router = useRouter();
+
+  const handleHomeClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-red-100">
+      <h1 className="text-3xl font-bold text-red-700 mb-4">
+        Payment Cancelled
+      </h1>
+      <p className="text-lg mb-8">
+        Your payment was cancelled. Please try again if you wish to book a
+        ticket.
+      </p>
+      <Button
+        onClick={handleHomeClick}
+        className="text-white px-6 py-3 rounded-lg shadow-md "
+      >
+        Go to Home
+      </Button>
+    </div>
+  );
 };
 
-export default page;
+export default TicketCancel;
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
-const page = () => {
+const TicketSuccess = () => {
+  const router = useRouter();
+
+  const handleHomeClick = () => {
+    router.push("/");
+  };
+
   return (
-    <div>Success</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-green-100">
+      <h1 className="text-3xl font-bold text-primary mb-4">
+        Payment Successful!
+      </h1>
+      <p className="text-lg mb-8">
+        Thank you for your purchase. Your ticket has been successfully booked.
+      </p>
+      <Button
+        onClick={handleHomeClick}
+        className="bg-primary text-white px-6 py-3 rounded-lg shadow-md "
+      >
+        Go to Home
+      </Button>
+    </div>
+  );
+};
 
-export default page
+export default TicketSuccess;
