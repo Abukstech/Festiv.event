15e33a9c6a76428b95f452017e2b378f4714451d
Abukstech <abdulmajeedbamidele4@gmail.com>
2024-07-17 13:19:30 +0100

    added serve host

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -4,5 +4,7 @@ import * as functions from "@/inngest/functions";
 
 export const { POST } = serve({
   client: inngest,
+  serveHost: "https://next-pxci-project.onrender.com",
+  servePath:"/api/inngest",
   functions: [...Object.values(functions)],
 });
