bff7b36090a9f499629019f9743934f74d9c8fba
Abukstech <abdulmajeedbamidele4@gmail.com>
2024-07-17 21:46:18 +0100

    made new updates

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/inngest/client";
+import { inngest } from "../../../inngest/client";
 import * as functions from "@/inngest/functions";
 
 export const { POST, GET, PUT } = serve({
   client: inngest,
-
   servePath: "/api/inngest",
+  signingKey: process.env.INNGEST_SIGNING_KEY,
   functions: [...Object.values(functions)],
 });
